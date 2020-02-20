266fedb26bd7bc0d82d969e82d19ef80b4a2fa3a
Andrea Rota <arota@ad.aule.afolmi.it>
2020-02-20 11:24:55 +0100

    aggiunta cartella src

---
+++ src/main/java/it/itsrizzoli/maven/progettomaven/App.java
@@ -0,0 +1,13 @@
+package it.itsrizzoli.maven.progettomaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/it/itsrizzoli/maven/progettomaven/EsempioConcatenazione.java
@@ -0,0 +1,13 @@
+package it.itsrizzoli.maven.progettomaven;
+
+public class EsempioConcatenazione {
+
+		public String concatenate(String string, String string2) {
+			return string+string2;
+		}
+		
+		public String getString(String stringa) {
+			return stringa;
+		}
+		
+}
